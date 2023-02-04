2ff00fc1baa5ff70b8fa5757139dfdc2ee566215
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-02-04 11:13:08 +0800

    Include font folder in bundling process

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,6 +17,7 @@ mix.js('resources/js/app.js', 'public/js')
     ])
     .copy('resources/css/main.css', 'public/css')
     .copyDirectory('resources/images', 'public/images')
+    .copyDirectory('resources/fonts', 'public/fonts')
     .version()
 
 mix.webpackConfig({
