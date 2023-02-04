d7c5ba7438b55eb46fd40751a746a26ecbcd5ef9
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-02-04 14:20:49 +0800

    Exclude unnecessary javascript

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -27,7 +27,7 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="{{ mix('css/main.css') }}">
     
-    <script src="{{ mix('js/app.js') }}" defer></script>
+    {{-- <script src="{{ mix('js/app.js') }}" defer></script> --}}
 </head>
 <body class="text-gray-900 dark:bg-pumpkin-dark dark:text-white">
     @yield('header')
